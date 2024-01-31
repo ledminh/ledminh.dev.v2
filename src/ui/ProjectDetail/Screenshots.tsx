d287d07e92a89e929fe5b5acd48fb330194a5538
Minh Le <minh.le@ledminh.dev>
2024-01-30 21:27:07 -0500

    Add ScreenshotItem with Screenshot fullscreeen

--- src/ui/ProjectDetail/Screenshots.tsx
+++ src/ui/ProjectDetail/Screenshots.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function Screenshots() {
@@ -8,12 +9,9 @@ export default function Screenshots() {
         {Array.from({ length: 6 }).map((_, index) => {
           return (
             <li key={index}>
-              <Image
+              <ScreenshotItem
                 src="https://picsum.photos/500/500"
                 alt="Landing Page"
-                width={500}
-                height={500}
-                className="rounded-lg w-20 h-20"
               />
             </li>
           );
@@ -22,3 +20,55 @@ export default function Screenshots() {
     </div>
   );
 }
+
+/***************************
+ * Sub-component
+ */
+
+const ScreenshotItem = (props: { src: string; alt: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        setIsOpen(false);
+      }
+    });
+
+    return () => {
+      document.removeEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+          setIsOpen(false);
+        }
+      });
+    };
+  }, []);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>
+        <Image
+          src={props.src}
+          alt={props.alt}
+          width={500}
+          height={500}
+          className="rounded-lg w-20 h-20 hover:transform hover:scale-110 transition-transform"
+        />
+      </button>
+      {isOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 z-50 flex justify-center items-center"
+          onClick={() => setIsOpen(false)}
+        >
+          <Image
+            src={props.src}
+            alt={props.alt}
+            width={500}
+            height={500}
+            className="rounded-lg"
+          />
+        </div>
+      )}
+    </>
+  );
+};
